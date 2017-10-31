4c85feb3416e7a8f841d9d454133547c84e5605c
baileywickham <baileywickham@gmail.com>
2017-10-31 11:35:00 -0700

    updated input thing

--- tensorflow/ml.py
+++ tensorflow/ml.py
@@ -2,16 +2,20 @@ import tensorflow as tf
 import os
 from PIL import Image
 files = os.listdir('/home/bailey/workspace/imagerec-ml/img') #make this relational so we can clone it and it will still work without being on just my computer. Lazy hack.
+graph = tf.Graph()
+
 
 #it looks like we are going to need to feed data. where there is a list of file names which get piped into a queue and then
 #decoded by a third piece, not sure on how to impliment any of this.
 
-if __name__ == '__main__':
-    for file in files:
-        print(file)
-        bw = tf.image.rgb_to_grayscale(tf.image.resize_images(tf.image.decode_jpeg(tf.read_file(file), channels=3), [299, 299]))
-        #this doesnt work and isn't the propper way to do it but its staying until I or andy gets around to fixing it
-with tf.Session() as sess:
-    print(sess.run(bw))
-    file = open("temp", 'w')
-    file.write(sess.run(bw))
+with graph.as_default():
+
+	#take an image list in as 64x64 with 3 color chanels. labels will be andy or not andy
+	images_ph = tf.placeholder(tf.float32, [None, 64, 64, 3])
+	labels_ph = tf.placeholder(tf.int8, [None]
+
+	images_flat = tf.contrib.layers.flatten(images_ph)
+	
+	#input to softmax, starts probabilties. not sure what tf.nn.relu does	
+	logits = tf.contrib.layers.fully_connected(images_flat, 2, tf.nn.relu)
+
