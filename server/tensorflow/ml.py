4c14cb1c685bf1f7d09110be7e010a3bda0fa570
baileywickham <baileywickham@gmail.com>
2017-11-09 08:41:25 -0800

    updated ml and read file

--- server/tensorflow/ml.py
+++ server/tensorflow/ml.py
@@ -4,75 +4,71 @@ import os
 import csv
 from PIL import Image
 
-#defines image directory as img, label as csv in label directory
+# defines image directory as img, label as csv in label directory
 data_dir = os.path.join(os.getcwd())
 labelNames = 'label/a.csv'
 graph = tf.Graph()
 
 
 def load_data(data_dir):
-	labels = []
-	images = []
-	
-	filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once("./img/*.jpeg"))
-	image_reader = tf.WholeFileReader()
+    labels = []
+    images = []
 
-	_, image_file = image_reader.read(filename_queue)
-	image = tf.image.decode_jpeg(image_file)
-	images.append(tf.image.resize_images(image,[64,64]))
+    filename_queue = tf.train.string_input_producer(tf.train.match_filenames_once("./img/*.jpeg"))
+    image_reader = tf.WholeFileReader()
+
+    _, image_file = image_reader.read(filename_queue)
+    image = tf.image.decode_jpeg(image_file)
+    images.append(tf.image.resize_images(image, [64, 64]))
+
+    # adds labels from csv to label array, retuyourns array
+    with open(labelNames, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            for i in range(len(row)):
+                labels.append(int(row[i]))
+
+    return images, labels
 
 
-	#adds labels from csv to label array, retuyourns array		
-	with open(labelNames, 'r') as csvfile:
-		reader = csv.reader(csvfile, delimiter=',')
-		for row in reader:
-			for i in range(len(row)):
-				labels.append(int(row[i]))
+def main():
+    images, labels = load_data(data_dir)
+    session = tf.Session(graph=graph)
+    session.run(init)
 
-	return images, labels
+    labels_a = np.array(labels)
+    images_a = np.array(images)
 
-images, labels = load_data(data_dir)
+    for i in range(201):
+        _, loss_value = session.run([train, loss], feed_dict={images_ph: images_a, labels_ph: labels_a})
 
+    if i % 10 == 0:
+        print("loss value: ", loss_value)
 
 
 with graph.as_default():
 
-	#take an image list in as 64x64 with 3 color chanels. labels will be andy or not andy
-	images_ph = tf.placeholder(tf.float32, [None, 64, 64, 3])
-	labels_ph = tf.placeholder(tf.int32, [None])
-	
-	#Flattens image into 1d vector
-	images_flat = tf.contrib.layers.flatten(images_ph)
-	
-	#input to softmax, starts probabilties. not sure what tf.nn.relu does	
-	logits = tf.contrib.layers.fully_connected(images_flat, 2, tf.nn.relu)
-	#converts labels to tensor
-	predicted_labels = tf.argmax(logits, 1)
-	
-	#defines loss function, cross entropy. For some reason it is better than mean squared.
-	loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels_ph))
-	
-	#Adam optimizer is a grad decent optimizer, telling it to minimize the loss function defined here.
-	train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
-	
-	#initalizes variables
-	init = tf.global_variables_initializer()
+    # take an image list in as 64x64 with 3 color chanels.
+    # labels will be 0 or not 1
+    images_ph = tf.placeholder(tf.float32, [None, 64, 64, 3])
+    labels_ph = tf.placeholder(tf.int32, [None])
 
-#sets session variable, inits vars
-session = tf.Session(graph=graph)
-session.run(init)	
-	
-with tf.Session() as sess:
-	
-	sess.run(tf.Print(images, [images]))
+    # Flattens image into 1d vector
+    images_flat = tf.contrib.layers.flatten(images_ph)
 
+    # input to softmax, starts probabilties. not sure what tf.nn.relu does
+    logits = tf.contrib.layers.fully_connected(images_flat, 2, tf.nn.relu)
+    # converts labels to tensor
+    predicted_labels = tf.argmax(logits, 1)
 
-labels_a = np.array(labels)
-images_a = np.array(images)
+    # defines loss function, cross entropy. it is better than mean squared.
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=labels_ph))
 
-#runs
-for i in range(201):
-	_, loss_value = session.run([train, loss], feed_dict={images_ph: images_a, labels_ph: labels_a})
-	
-	if i % 10 == 0:
-		print("loss value: ", loss_value)
+    # Adam optimizer is a grad decent optimizer,
+    train = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+
+    # initalizes variables
+    init = tf.global_variables_initializer()
+
+if __name__ == "__main__":
+    main()
--- server/tensorflow/read.py
+++ server/tensorflow/read.py
@@ -1,26 +1,36 @@
 import tensorflow as tf
-import os
-import csv
 
-data_dir = os.path.join(os.getcwd(), 'img')
-labelNames = 'label/a.csv'
+# Make a queue of file names including all the JPEG images files in the relative
+# image directory.
+filename_queue = tf.train.string_input_producer(
+    tf.train.match_filenames_once("./images/*.jpg"))
 
-graph = tf.Graph()
-def load_data(data_dir):
-	labels = []
-	images = []	
-	with open(labelNames, 'r') as csvfile:
-		reader = csv.reader(csvfile, delimiter=',')
-		for row in reader:
-			for i in range(len(row)):
-				labels.append(int(row[i]))
-	filename_queue = tf.train.string_input_producer(
-    tf.train.match_filenames_once("./images/*.jpeg"))
+# Read an entire image file which is required since they're JPEGs, if the images
+# are too large they could be split in advance to smaller files or use the Fixed
+# reader to split up the file.
+image_reader = tf.WholeFileReader()
 
-	image_reader = tf.WholeFileReader()
-	_, image_file = image_reader.read(filename_queue)
+# Read a whole file from the queue, the first returned value in the tuple is the
+# filename which we are ignoring.
+_, image_file = image_reader.read(filename_queue)
 
-	print('hello')
-	print(labels, images)
+# Decode the image as a JPEG file, this will turn it into a Tensor which we can
+# then use in training.
+image = tf.image.decode_jpeg(image_file)
 
-load_data(data_dir)
+# Start a new session to show example output.
+with tf.Session() as sess:
+    # Required to get the filename matching to run.
+    tf.initialize_all_variables().run()
+
+    # Coordinate the loading of image files.
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(coord=coord)
+
+    # Get an image tensor and print its value.
+    image_tensor = sess.run([image])
+    print(image_tensor)
+
+    # Finish off the filename queue coordinator.
+    coord.request_stop()
+    coord.join(threads)
