8855b440d6aeb48d43db023119c43cedaae08831
Keeganfn <Keegan.nave@gmail.com>
2017-11-15 08:27:21 -0800

    updated split; todo: add compression

--- server/split.py
+++ server/split.py
@@ -3,6 +3,7 @@ from videosequence import VideoSequence
 import os
 import glob
 import shutil
+import csv
 
 mp4File = input("Enter name of mp4 file:")
 
@@ -10,6 +11,7 @@ src_dir = input("Enter full path of the folder split.py is held in:")
 
 dst_dir = input("Enter full path of the intended folder for jpg's:")
 
+andyornot = input("Enter 1 if all images are andy and 0 if not:")
 
 with closing(VideoSequence(mp4File)) as frames:
     for idx, frame in enumerate(frames[1:]):
@@ -17,9 +19,16 @@ with closing(VideoSequence(mp4File)) as frames:
 
 
 
+with open('labels.csv','w') as myfile:
+   wrtr = csv.writer(myfile, delimiter=',', quotechar='"')
+   for jpgfile in glob.iglob(os.path.join(src_dir, "*.jpg")):
+        wrtr.writerow(andyornot)
+        myfile.flush()
 
 
 for jpgfile in glob.iglob(os.path.join(src_dir, "*.jpg")):
     shutil.copy(jpgfile, dst_dir)
     os.remove(jpgfile)
+
+
 
