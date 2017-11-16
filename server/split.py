c653da79cb4a11b7248e2f44767ad8608d169505
bailey <baileywickham@gmail.com>
2017-11-16 08:42:48 -0800

    updated split, cleaned up code.y

--- server/split.py
+++ server/split.py
@@ -5,30 +5,24 @@ import glob
 import shutil
 import csv
 from skimage import transform
+import sys
 
-mp4File = input("Enter name of mp4 file:")
-
-src_dir = input("Enter full path of the folder split.py is held in:")
-
-dst_dir = input("Enter full path of the intended folder for jpg's:")
-
+try:
+    mp4File = str(sys.argv)
+except OSError:
+    print("make sure to pass a mp4 in as a paramter")
+     
 andyornot = input("Enter 1 if all images are andy and 0 if not:")
 
 with closing(VideoSequence(mp4File)) as frames:
     for idx, frame in enumerate(frames[1:]):
-        frame.save("frame{:}.jpg".format(idx))
+        frame.save("images/frame{:}.jpg".format(idx))
 
 
 
 with open('labels.csv','w') as myfile:
    wrtr = csv.writer(myfile, delimiter=',', quotechar='"')
-   for jpgfile in glob.iglob(os.path.join(src_dir, "*.jpg")):
+   for jpgfile in glob.iglob(os.path.join('images/', "*.jpg")):
         wrtr.writerow(andyornot)
         myfile.flush()
-
-
-for jpgfile in glob.iglob(os.path.join(src_dir, "*.jpg")):
-    im = skimage.transform.resize(im, (64, 64))
-    shutil.copy(jpgfile, dst_dir)
-    os.remove(jpgfile)
 
